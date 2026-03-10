868e6f354c43331161bafc5d6407bbee104373fd
anupkadam <anupkadam01@gmail.com>
2026-03-10 22:24:15 +0530

    Update Navbar.jsx
    Signed-off-by: anupkadam <anupkadam01@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,19 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-900 text-white p-4">
-      <div className="max-w-6xl mx-auto flex justify-between">
+    <nav className="bg-white shadow">
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
 
-        <h1 className="text-xl font-bold">
+        <h1 className="text-xl font-bold text-blue-800">
           Academic Portal
         </h1>
 
         <div className="space-x-6">
           <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
           <Link to="/courses">Courses</Link>
           <Link to="/materials">Materials</Link>
           <Link to="/blog">Blog</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
         </div>
 
       </div>
