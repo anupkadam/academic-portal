5474f420e775e4a1dee40102f551ee65352750b6
anupkadam <anupkadam01@gmail.com>
2026-03-10 22:24:51 +0530

    Update Hero.jsx
    Signed-off-by: anupkadam <anupkadam01@gmail.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 export default function Hero() {
   return (
-    <section className="text-center py-20 bg-gray-100">
+    <section className="bg-blue-900 text-white py-24 text-center">
 
-      <h1 className="text-5xl font-bold text-blue-900">
-        Academic Learning Portal
+      <h1 className="text-5xl font-bold">
+        Academic Knowledge Portal
       </h1>
 
-      <p className="mt-6 text-lg text-gray-600">
-        Lecture notes, assignments, AI resources, and research materials.
+      <p className="mt-4 text-lg">
+        Lecture Notes • Assignments • AI Research • Learning Resources
       </p>
 
-      <button className="mt-8 bg-blue-600 text-white px-6 py-3 rounded-lg">
+      <button className="mt-8 bg-white text-blue-900 px-6 py-3 rounded-lg">
         Explore Courses
       </button>
 
