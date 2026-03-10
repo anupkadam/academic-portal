57a449dad92b481f76cc52b733ef68b5a582b2c7
anupkadam <anupkadam01@gmail.com>
2026-03-10 22:50:39 +0530

    Update App.jsx
    Signed-off-by: anupkadam <anupkadam01@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { HashRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import AppRoutes from "./router/AppRoutes";
 
 function App() {
   return (
     <HashRouter>
+
       <Navbar />
+
       <AppRoutes />
+
+      <Footer />
+
     </HashRouter>
   );
 }
