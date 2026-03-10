4fcd68427b7df4328cfcf3bd8f4d381f622d2753
anupkadam <anupkadam01@gmail.com>
2026-03-10 22:48:21 +0530

    Create Footer.jsx
    Signed-off-by: anupkadam <anupkadam01@gmail.com>

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-6">
+
+      <p>© 2026 Academic Knowledge Portal - Anup Kadam</p>
+
+    </footer>
+  );
+}
